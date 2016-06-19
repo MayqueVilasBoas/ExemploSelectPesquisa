22b575e645432c9aee56b9c43d93118e2fb8cac9
Mayque <mayque_vb@hotmail.com>
2016-06-19 17:56:15 -0300

    Exemplo de utilização de select com pesquisa.

---
+++ js/controller/opcoesController.js
@@ -0,0 +1,9 @@
+
+app.controller('opcoesController', function($scope) {
+
+  $scope.opcoes = [{idOpcao: 1, nome: "Opção 1"},
+                   {idOpcao: 2, nome: "Opção 2"},
+                   {idOpcao: 3, nome: "Opção 3"},
+                   {idOpcao: 4, nome: "Opção 4"},
+                   {idOpcao: 5, nome: "Opção 5"}];
+});
